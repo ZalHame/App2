469b7b31aee184579a2b99ea987e9068796914f5
ZalHame <zalialovmurat@yandex.ru>
2023-01-24 17:41:41 +0300

    Добавьте файлы проекта.

---
+++ App2/App2/Views/AboutPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btn_SignIn_Click(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new TabbedPage1());
+        }
+
+    }
+}
